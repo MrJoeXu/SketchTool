ad8ebbe4277303a6a1b898afa417e3478e0cb852
MrJoeXu <zxu3@wpi.edu>
2015-05-18 16:36:48 -0400

    add KeyControll functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,82 +1,107 @@
-$(document).ready(function() {
-	function initGrid() {
-		for (var i=1; i<= 40; i++) {
-			for (var j=1; j<=40; j++){
-				$('#board').append('<div class="tile"></div>')
-			};
+
+var color = '#000000';
+
+function initGrid(gridSize) {
+	var length = 640/gridSize;
+
+	for (var i=1; i<= gridSize*gridSize; i++) {
+		//for (var j=1; j<=gridSize; j++){		
+			$('#board').append('<div class="tile"></div>')
+		//};
+	};
+		$('.tile').css(
+		{'width': length, 
+		 'height': length});
+	console.log(i);
+	console.log(i);
+};
+
+
+
+function hoverGrid() {
+	$('.tile').hover(function() {
+		$(this).css('background-color', color);
+		var opacity = Number($(this).css('opacity'));
+		if (opacity <= 1) {
+			$(this).css('opacity', opacity + 0.1);
 		};
-	};
 
-	$(document).keydown(function(event){ 
-console.log(event.keyCode); 
-}); 
+	}, function() {
+	});
+};
 
-	keyControll();
-	initGrid();
+function eraseGrid() {
+	$('.tile').hover(function() {
+		var opacity = Number($(this).css('opacity'));
+			$(this).css('opacity', 0);
+	}, function() {
+	});
+};
 
-	function keyControll() {
-		$(document).keydown(function(event) {
-			if (event.keyCode == 16) {
-				hoverGrid();
-			}
-		});
-		$(document).keyup(function(event) {
-			if (event.keyCode == 16) {
-				$('.tile').unbind("mouseenter mouseleave");
-			}
-		});
-
-		$(document).keydown(function(event) {
-			if (event.keyCode == 69) {
-				eraseGrid();
-			}
-		});
-		$(document).keyup(function(event) {
-			if (event.keyCode == 69) {
-				$('.tile').unbind("mouseenter mouseleave");
-			}
-		});
-
-	};
-
-	function hoverGrid() {
-		$('.tile').hover(function() {
-			var opacity = Number($(this).css('opacity'));
-			if (opacity <= 1) {
-				$(this).css('opacity', opacity + 0.1);
-			};
-
-		}, function() {
-		});
-	};
-
-	function eraseGrid() {
-		$('.tile').hover(function() {
-			var opacity = Number($(this).css('opacity'));
-			if (opacity >= 0.4) {
-				$(this).css('opacity', opacity - 0.1);
-			};
-
-		}, function() {
-		});
-	};
-
-	function newGrid(gridSize) {
-		//$('.tile').remove();
-		var length = 640/gridSize;
-		$('.tile').css('width', length);
-		for (var i=1; i<= gridSize; i++) {
-			for (var j=1; j<= gridSize; j++){
-				$('#board').append('<div class="tile"></div>')
-			};
-		};
-		//hoverGrid();
-	}
-
-
-	$('#reset').click(function(event) {
-		$('.tile').remove();
-		initGrid();
+function initKeyControll() {
+	// Press left 'shift' to start drawing, realse to stop;
+	$(document).keydown(function(event) {
+		if (event.keyCode == 16) {
+			hoverGrid();
+		}
+	});
+	$(document).keyup(function(event) {
+		if (event.keyCode == 16) {
+			$('.tile').unbind("mouseenter mouseleave");
+		}
+	});
+	// Press "e" to rease tile, release to cancel;
+	$(document).keydown(function(event) {
+		if (event.keyCode == 69) {
+			eraseGrid();
+		}
+	});
+	$(document).keyup(function(event) {
+		if (event.keyCode == 69) {
+			$('.tile').unbind("mouseenter mouseleave");
+		}
 	});
 
+};
+
+$('#reset').click(function(event) {
+	$('.tile').remove();
+	initGrid();
+});
+
+
+
+$('#red').click(function(event) {
+	color = '#E9223C';
+});
+$('#orange').click(function(event) {
+	color = '#F1AE68';
+});
+$('#yellow').click(function(event) {
+	color = '#FFCE35';
+});
+$('#green').click(function(event) {
+	color = '#39CB74';
+});
+$('#cyan').click(function(event) {
+	color = '#3FD3B8';
+});
+$('#blue').click(function(event) {
+	color = '#3B9AD9';
+});
+$('#purple').click(function(event) {
+	color = '#612268';
+});
+
+$(document).ready(function() {
+	initGrid(16);
+	initKeyControll();
+
+
+
+	$('#size').click(function(event) {
+		var input = prompt("Please Enter the numer of tiles for sides of board: \n (less than 100) ");
+		$('.tile').remove();
+		initGrid(input);
+	});	
 });
